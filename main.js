253efefa1cc0c0d4f6b1bb463cc9d4613de688e0
Guillermo Martín <sharp.guille@gmail.com>
2023-09-02 17:14:23 -0300

    por las dudas

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 function createProductElement(product) {
     return `
-        <div class="product">
+        <div class="product" data-product-id="${product.id}">
             <img src="${product.image}">
             <div class="box">
                 <span class="title">${product.title}</span>
-                <span class="price">U$D ${product.price}</span>
+                <div class="price-wishlist">
+                    <span class="price">U$D ${product.price}</span>
+                    <label class="ui-bookmark">
+                        <input type="checkbox">
+                        <div class="bookmark">
+                            <i class="bi bi-bookmark-plus-fill" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Add to wishlist" data-bs-custom-class="custom-tooltip"></i>
+                            <i class="bi bi-bookmark-dash-fill" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Remove from wishlist" data-bs-custom-class="custom-tooltip"></i>
+                        </div>
+                    </label>
+                </div>
                 <div class="buttons">
-                    <button class="CartBtn">
+                    <button class="CartBtn" onclick="addToCart(${product.id})">
                         <span class="IconContainer"> 
                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="#FFF" class="cart">
                                 <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
@@ -26,14 +35,24 @@ function createProductElement(product) {
     `
 }
 
+try {
+    fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(json => {
+            const itemsContainer = document.getElementById("items")
+            itemsContainer.innerHTML = ""
+            json.forEach(product => {
+                const productHTML = createProductElement(product)
+                itemsContainer.innerHTML += productHTML
+            })
 
-fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(json => {
-        const itemsContainer = document.getElementById("items")
-        itemsContainer.innerHTML = ""
-        json.forEach(product => {
-            const productHTML = createProductElement(product)
-            itemsContainer.innerHTML += productHTML
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
         })
-    })
+} catch {
+
+}
+
+function addToCart(productId) {
+    console.log(productId)
+}
